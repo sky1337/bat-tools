dea323c9ccd688488a3357f3cd4df402e3e9ec34
sky1337 <ryancraig2002@gmail.com>
2017-05-02 12:51:58 -0500

    CommandTerminal.bat

---
+++ CommandTerminal.bat
@@ -0,0 +1,11 @@
+@echo off
+title Command Executer
+color b
+
+echo Command Executer by: Sky//PirateSec .)
+echo #######################################
+: execute
+echo Please Type A Command Here:
+set /p cmd=Command:
+%cmd%
+goto execute
